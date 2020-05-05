1a23599f58c2644308a1cc612ff8904583ee6e6b
ccushnie <Corey.Cushnie@yahoo.com>
2020-05-04 21:06:33 -0400

    Updates

---
+++ precache-manifest.7fc578bcc2788445741a0a2ee87294d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea63b67c4f206186cc338093c77da226",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "1a34468994241fb83421",
+    "url": "/monstersrolodex/static/css/main.c0b66f79.chunk.css"
+  },
+  {
+    "revision": "1b295834338ff94bad5f",
+    "url": "/monstersrolodex/static/js/2.745272f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.745272f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a34468994241fb83421",
+    "url": "/monstersrolodex/static/js/main.63355034.chunk.js"
+  },
+  {
+    "revision": "55bb22e6862d42459986",
+    "url": "/monstersrolodex/static/js/runtime-main.cf30ea39.js"
+  }
+]);
